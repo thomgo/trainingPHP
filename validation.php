066ed32e59d886cedcd571c025fedb28c288affe
Thomas Gossart <tg.59290@gmail.com>
2016-09-29 11:06:20 +0200

    added first error check in validation.php

--- validation.php
+++ validation.php
@@ -1,4 +1,14 @@
 <?php
 
+if(empty($_POST['name']) || empty($_POST['firstname']) || empty($_POST['age']) || empty($_POST['city']) || !isset($_POST['hobbies'])) {
+    header('Location: formulaire.php?type=error&code=1');
+  }
+  else {
+    header('Location: formulaire.php?type=error&code=2');
+  }
+
+// else {
+//   header('Location: formulaire.php?type=error&code=1');
+// }
 
  ?>

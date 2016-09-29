93e50a769fe466b7b7f6fc984795d80dbebe258d
Thomas Gossart <tg.59290@gmail.com>
2016-09-29 12:10:38 +0200

    added the content and the file validation in validation.php

--- validation.php
+++ validation.php
@@ -1,11 +1,28 @@
 <?php
 
-if(empty($_POST['name']) || empty($_POST['firstname']) || empty($_POST['age']) || empty($_POST['city']) || !isset($_POST['hobbies'])) {
-    header('Location: formulaire.php?type=error&code=1');
-  }
-  else {
-    header('Location: formulaire.php?type=error&code=2');
-  }
+ if(empty($_POST['name']) || empty($_POST['firstname']) || empty($_POST['age']) || empty($_POST['city']) || empty($_POST['gender']) || !isset($_POST['hobbies'])) {
+     header('Location: formulaire.php?type=error&code=1');
+   }
+
+   elseif (isset($_FILES['file']) AND $_FILES['file']['error'] == 0) {
+
+     if($_FILES['file']['size'] <= 1000000) {
+
+       $fichier = pathinfo($_FILES['file']['name']);
+       $extension = $fichier['extension'];
+       $extensionOk = array('jpeg', 'jpg', 'gif', 'png', 'pdf', 'doc');
+
+          if (in_array($extension, $extensionOk)) {
+            move_uploaded_file($_FILES['file']['tmp_name'], 'files/' . basename($_FILES['file']['name']));
+          }
+     }
+     header('Location: formulaire.php?type=error&code=2');
+   }
+
+
+   else {
+     header('Location: formulaire.php?type=success&code=1');
+ }
 
 // else {
 //   header('Location: formulaire.php?type=error&code=1');

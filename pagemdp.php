61eb8822f3b50c4dd278d34f1833047f789216fa
Thomas Gossart <tg.59290@gmail.com>
2016-09-29 16:17:46 +0200

    added the form and the test for the pasword and the display of the code in pagemdp.php

--- pagemdp.php
+++ pagemdp.php
@@ -1,14 +1,31 @@
 <html>
 
 <head>
-<title>Exo php</title>
+<title>Exercice de mot de passe</title>
 </head>
 
 <body>
 
 <?php
 
+if (!isset($_POST['password']) OR $_POST['password'] != "kangourou") {
 
+echo '<form action="pagemdp.php" method="POST">
+
+<label>Mot de passe : <input type="password" name="password"/></label>
+<p><input type="submit" value="Voir les codes"></p>
+
+</form>';
+}
+
+else {
+  
+echo '<p>Vous accédez à un espace strictement réservé au personnel, les codes suivants sont
+très importants, d\'eux dépend l\'avenir de Simplon !!!!</p>';
+
+
+echo '<p>Voici les codes...</p>';
+}
 
 ?>
 

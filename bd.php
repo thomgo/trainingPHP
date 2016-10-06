0a0ef5cdbdda7c94bc75989622cd75e3a3f05663
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 11:12:17 +0200

    added the database connection in db.php

--- bd.php
+++ bd.php
@@ -1,4 +1,12 @@
 <?php
 
+// Try to connect to the database
+try {
+  $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'ThomAdmin12');
+}
 
+// If the connection is impossible get the error message
+catch (Exception $exception) {
+  die ('erreur :'. $exception->getMessage());
+}
  ?>

146f615431ce3fa390676894c716debab4a3c657
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 11:17:29 +0200

    added the database information insertion in minichat_treatment.php

--- minichat_treatment.php
+++ minichat_treatment.php
@@ -2,4 +2,9 @@
 
 require ('bd.php');
 
+$request = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(?, ?)');
+$request->execute(array(htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['message'])));
+
+header('Location: minichat.php');
+
  ?>

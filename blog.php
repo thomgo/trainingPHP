dc4acb29d41aaa1c4d99ed21d290f06b52aa1855
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 16:14:45 +0200

    added the sql request to get the articles and display it in html in blog.php

--- blog.php
+++ blog.php
@@ -11,4 +11,40 @@
         <h1>Un blog avec des commentaires</h1>
         <p>Les articles</p>
 
+        <?php
+
+          require ('db.php');
+
+          $requete = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%Hh%imin le %d/%m/%Y\') AS datecrea FROM billets ORDER BY date_creation DESC LIMIT 5');
+
+          while ($billets = $requete->fetch())
+          {
+         ?>
+
+         <div class="news">
+             <h3>
+                 <?php echo htmlspecialchars($billets['titre']);
+                 echo '<p class="date"> à' . $billets['datecrea'] . '</p>';
+                 ?>
+             </h3>
+
+             <p>
+             <?php
+             echo htmlspecialchars($billets['contenu']);
+             ?>
+             <br />
+
+             <?php echo '<a href="commentaires.php?billet='. $billets['id'] . '">Voir les commentaires</a>';?>
+             </p>
+
+         </div>
+
+         <?php
+         }
+
+         $requete->closeCursor();
+         ?>
+
     </body>
+
+    </html>

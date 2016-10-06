9855e619d3dd4e1633b351de9077d80d6caaf897
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 12:10:23 +0200

    added the delete request in minichat_treatment.php

--- minichat_treatment.php
+++ minichat_treatment.php
@@ -6,10 +6,14 @@ require ('bd.php');
 // Protect the data from the form
 $pseudo = htmlspecialchars($_POST['pseudo']);
 $message = htmlspecialchars($_POST['message']);
+$code = (int) $_POST['code'];
+$supcode = (int) $_POST['supcode'];
 
 // Prepare and insert the data
-$request = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(?, ?)');
-$request->execute(array($pseudo, $message));
+$request = $bdd->prepare('INSERT INTO minichat(pseudo, message, code) VALUES(?, ?, ?)');
+$request->execute(array($pseudo, $message, $code));
+
+$bdd->exec('DELETE FROM minichat WHERE code = ' . $supcode);
 
 header('Location: minichat.php');
 

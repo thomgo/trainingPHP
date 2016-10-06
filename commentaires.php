6faafcab280b310723fe90e08156d9468358d2a6
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 16:18:06 +0200

    added the basic html strucuture and the connection to database in commentaires.php

--- commentaires.php
+++ commentaires.php
@@ -1,4 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Un blog</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+
+        <h1>Un blog avec des commentaires</h1>
+        <p><a href="blog.php">Vers la liste des articles</a></p>
+
 <?php
 
+    require ('db.php');
 
- ?>
+
+?>
+
+</body>
+
+</html>

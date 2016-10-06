5e1453c21ce78dc3d5d0f509c9865fb96aee8ca0
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 15:25:00 +0200

    added the html base and main title in blog.php

--- blog.php
+++ blog.php
@@ -1,4 +1,14 @@
-<?php
+<html>
 
+    <head>
+        <meta charset="utf-8" />
+        <title>Un blog</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
 
- ?>
+    <body>
+
+        <h1>Un blog avec des commentaires</h1>
+        <p>Les articles</p>
+
+    </body>

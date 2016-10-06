5c1ca827779fc873945db5df26b98bfa6cfbe3f0
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 11:26:01 +0200

    treated the variables from the form with htmlspecialchars before the sql request in minichat_treatment.php

--- minichat_treatment.php
+++ minichat_treatment.php
@@ -2,8 +2,11 @@
 
 require ('bd.php');
 
+$pseudo = htmlspecialchars($_POST['pseudo']);
+$message = htmlspecialchars($_POST['message']);
+
 $request = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(?, ?)');
-$request->execute(array(htmlspecialchars($_POST['pseudo']), htmlspecialchars($_POST['message'])));
+$request->execute(array($pseudo, $message));
 
 header('Location: minichat.php');
 

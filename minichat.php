236df4b2d772ffc19a4e92ad333639b5b3f57ef7
Thomas Gossart <tg.59290@gmail.com>
2016-10-06 11:13:51 +0200

    added the form in minichat.php

--- minichat.php
+++ minichat.php
@@ -1,4 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Minichat</title>
+    </head>
+
+    <form action="minichat_treatment.php" method="POST">
+
+    <label>Votre pseudo : <input type="text" name="pseudo"/></label>
+    <label>Votre Message : <input type="text" name="message"/></label>
+
+    <p><input type="submit" value="Envoyer"></p>
+
+    </form>
+
 <?php
 
-
  ?>
+
+ </html>

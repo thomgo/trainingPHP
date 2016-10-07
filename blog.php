194ee4b45c3df17e60382b0496aadc26e74bb3cf
Thomas Gossart <tg.59290@gmail.com>
2016-10-07 15:20:32 +0200

    added comment and an htmlspecialchar to the get methode in blog.php

--- blog.php
+++ blog.php
@@ -13,35 +13,40 @@
 
         <?php
 
+// Connexion to the database
           require ('db.php');
 
+// Get the articles in the database
           $requete = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%Hh%imin le %d/%m/%Y\') AS datecrea FROM billets ORDER BY date_creation DESC LIMIT 5');
 
+// Get an array of data and display the content of each article
           while ($billets = $requete->fetch())
           {
          ?>
 
          <div class="news">
+
+           <!-- title and date -->
              <h3>
                  <?php echo htmlspecialchars($billets['titre']);
                  echo '<p class="date"> à' . $billets['datecrea'] . '</p>';
                  ?>
              </h3>
 
-             <p>
+            <!-- Content -->
              <?php
-             echo htmlspecialchars($billets['contenu']);
-             ?>
-             <br />
 
-             <?php echo '<a href="commentaires.php?billet='. $billets['id'] . '">Voir les commentaires</a>';?>
-             </p>
+             echo "<p>". htmlspecialchars($billets['contenu']) . "</p>";
+             echo '<a href="commentaires.php?billet='. $billets['id'] . '">Voir les commentaires</a>';
+
+             ?>
 
          </div>
 
          <?php
          }
 
+// Close the request
          $requete->closeCursor();
          ?>
 

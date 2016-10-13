d96ba7eb750713b2f9eef04556a6750b1a9310b0
Thomas Gossart <tg.59290@gmail.com>
2016-10-13 13:59:39 +0200

    added message for email and password problem in enregistrement.php

--- enregistrement.php
+++ enregistrement.php
@@ -9,4 +9,13 @@ if (empty($pseudo) && empty($mdp) && empty($mdpconfirm) && empty($mail)) {
   header("Location: inscription.php?code=1");
 }
 
+if ($mdp != $mdpconfirm) {
+  header("Location: inscription.php?code=2");
+}
+
+if (!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $mail)) {
+  header("Location: inscription.php?code=3");
+}
+
+
  ?>

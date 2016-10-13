4d698d5a00693eb15c7a83b8e34bc5981c3a3db2
Thomas Gossart <tg.59290@gmail.com>
2016-10-13 15:46:22 +0200

    added the form in connexion.php

--- connexion.php
+++ connexion.php
@@ -11,7 +11,30 @@
       <?php
       // Connexion to the database
                 require ('db.php');
+
+                if (!empty($_GET['code'])) {
+                  if ($_GET['code'] == 1) {
+                    echo "<p>Il est l'heure de vous connecter pour la première fois</p>";
+                  }
+                }
        ?>
+
+       <!-- Html form -->
+              <form action="enregistrement.php" method="post">
+
+                  <label>Votre pseudo
+                    <p>
+                      <input type="text" name="pseudo">
+                    </p>
+                  </label>
+                   <label>Mot de passe
+                    <p>
+                      <input type="password" name="mdp">
+                    </p>
+                  </label>
+
+                <input type="submit" value="Me connecter">
+              </form>
 
     </body>
 

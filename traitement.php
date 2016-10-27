8772d3671b505b124d1521c4784137a739ac6720
Thomas Gossart <tg.59290@gmail.com>
2016-10-27 11:20:02 +0200

    added the insertion in db of tasks and lists

--- traitement.php
+++ traitement.php
@@ -1,6 +1,22 @@
 <?php
-
 // Connexion to the database
-          require ('db.php');
+    require ('db.php');
 
+if (isset($_POST['list'])) {
+  $list = htmlspecialchars($_POST['list']);
+
+  $request = $bdd->prepare("INSERT INTO list(nom) VALUES (?)");
+  $request->execute(array($list));
+}
+
+if (!empty($_POST['taches'])) {
+  $taches = htmlspecialchars($_POST['taches']);
+  $listid = (int)$_POST['listid'];
+
+  $requete = $bdd->prepare("INSERT INTO taches(nom, listid, datecrea) VALUES (?, ?, NOW())");
+  $requete->execute(array($taches, $listid));
+}
+
+
+header('Location: index.php');
  ?>
